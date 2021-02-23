d4d7f5e31932888a5d7d38ac10b8c3aae8bd563a
dam <dam@10.100.100.15>
2021-02-23 13:17:12 +0100

    1 2

---
+++ src/refactorizar/Clase2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refactorizar;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    int b;
+    int c;
+    
+    public int resta(){
+        return b-c;
+    }
+}
